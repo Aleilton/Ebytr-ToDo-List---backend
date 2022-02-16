f72de003e747e2688596c0a545122723c522d272
Aleilton Chavenco <aleilton@gmail.com>
2022-02-15 22:47:54 -0300

    feat(users): GetUserByEmail models

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,11 +1,20 @@
 const { getConnection } = require('./connections');
 
+const COLLECTION_NAME = 'users';
+
 const create = async (name, password, role, email) => {
   const conn = await getConnection();
-  const result = await conn.collection('users').insertOne({ name, password, role, email });
+  const result = await conn.collection(COLLECTION_NAME).insertOne({ name, password, role, email });
+  return result;
+};
+
+const getUserByEmail = async (email) => {
+  const conn = await getConnection();
+  const result = await conn.collection(COLLECTION_NAME).findOne({ email });
   return result;
 };
 
 module.exports = {
   create,
+  getUserByEmail,
 };
