34b6a1e35d87208b0853b80d4050438217cf7647
Aleilton Chavenco <aleilton@gmail.com>
2022-02-15 22:53:14 -0300

    feat(users/create): create services

---
+++ backend/services/users.js
@@ -0,0 +1,30 @@
+const Joi = require('@hapi/joi');
+
+const usersModels = require('../models/users');
+
+const { errorObject } = require('../utils/errorObject');
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  password: Joi.string().required(),
+  role: Joi.string().required(),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+});
+
+const create = async (name, password, role, email) => {
+  const { error } = userSchema.validate({ name, password, role, email });
+  if (error) {
+    const err = new Error(error.details.map((errorObj) => errorObj.message).toString());
+    err.code = 'invalid_data';
+    err.status = 400;
+    throw (err);
+  }
+
+  const emailAlreadyExists = await usersModels.getUserByEmail(email);
+  if (emailAlreadyExists) {
+    throw (errorObject('Email already registered', 409));
+  }
+
+  const newUser = await usersModels.create(name, password, role, email);
+  return newUser.ops[0];
+};
