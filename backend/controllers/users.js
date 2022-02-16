68598b146e55f19163d6656d72e771ae33b06d45
Aleilton Chavenco <aleilton@gmail.com>
2022-02-16 08:12:18 -0300

    feat(users/create): create controllers Users name correction

---
+++ backend/controllers/users.js
@@ -0,0 +1,19 @@
+const rescue = require('express-rescue');
+
+const userService = require('../services/users');
+
+const create = rescue(async (req, res, next) => {
+  try {
+    const { name, password, role, email} = req.body;
+    const newUser = await userService.create(name, password, role, email);
+    delete newUser.password;
+    return res.status(201).json({ user: { ...newUser } });
+  } catch (error) {
+    console.log(`ERROR: POST createUser => ${error.message}`);
+    return next(error);
+  }
+});
+
+module.exports = {
+  create,
+};
