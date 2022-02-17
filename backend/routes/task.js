ff7e0975f5bfb80aa4717b921b6df891afb5596e
Aleilton Chavenco <aleilton@gmail.com>
2022-02-16 23:32:42 -0300

    feat(tasks): Delete router

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -5,11 +5,12 @@ const taskRouter = express.Router();
 const auth = require('../middlewares/auth');
 
 const {
-  create, getTasksByUserId, update,
+  create, getTasksByUserId, update, deleteTask,
 } = require('../controllers/tasks');
 
 taskRouter.post('/', auth, create);
 taskRouter.get('/', auth, getTasksByUserId);
 taskRouter.put('/:id', auth, update);
+taskRouter.delete('/:id', auth, deleteTask);
 
 module.exports = taskRouter;
